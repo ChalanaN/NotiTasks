308d512d5fb904032b0439214f0ec9edcbdfb096
Chalana Nethsara <wchalanaw@gmail.com>
2023-10-14 23:18:59 +0530

    Ability to set time also

--- src/parser.ts
+++ src/parser.ts
@@ -1,10 +1,17 @@
 import extractDate from "extract-date"
+import extractTime from "extract-time"
 import { NotionTask } from "."
 
 const REGEXPS = {
     workspace: /#(\w+)/,
     project: /\[((?:\s|\w)+)\]/,
     title: /(.*?)\s*(?:(?:by)|,|(?:from.+to\s)|$)/
+},
+extractDateArgs = {
+    locale: "en",
+    timezone: "Asia/Colombo",
+    minimumAge: 2,
+    maximumAge: 4
 }
 
 const parseMessage = (msg: string): NotionTask => {
@@ -16,16 +23,19 @@ const parseMessage = (msg: string): NotionTask => {
 
     let title = msg.match(REGEXPS.title)?.[1]?.trim()
 
-    let extractedDate = extractDate(msg, {
-            locale: "en",
-            timezone: "Asia/Colombo",
-            minimumAge: 2,
-            maximumAge: 4
-        }),
+    let extractedDate = extractDate(msg, extractDateArgs),
+        extractedTime = extractTime(msg),
         date = {
             start: extractedDate[0]?.date,
             end: extractedDate[1]?.date
         }
+
+    if (extractedTime.length > 0) {
+        const todayDate = extractDate("today", extractDateArgs)[0].date
+
+        date.start = `${ date.start || todayDate }T${ extractedTime[0].time }+05:30`
+        if (extractedTime[1]) date.end = `${ date.end || todayDate }T${ extractedTime[1].time }+05:30`
+    }
 
     return {
         title,
